08dcb6297c27b260238c79cff511d75f3cdad27c
haywaii1 <ayotundeonasanya@yahoo.com>
2025-03-15 14:54:50 +0100

    LeamLegal

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AppointmentController;
+
+
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/appointment', [AppointmentController::class, 'appointment'])->name('appointment');
+
